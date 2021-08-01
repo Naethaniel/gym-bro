0c16289b025a85518a8ac3e2c9523411a2b5a938
Naethaniel <bljuuzeff@gmail.com>
2021-08-01 17:53:20 +0200

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DbService } from 'src/app/services/db.service';
 import { SQLite } from '@ionic-native/sqlite/ngx';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SQLite, DbService],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SQLite,
+    DbService,
+    HttpClientModule
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,45 +1,44 @@
 import { Injectable } from '@angular/core';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { ReplaySubject } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbService {
-  private db: SQLiteObject;
+  private db: ReplaySubject<SQLiteObject> = new ReplaySubject<SQLiteObject>();
 
-  constructor(private sqlite: SQLite) {
+  constructor(
+    private sqlite: SQLite,
+    private http: HttpClient
+  ) {
+    this.sqlite.create({
+      name: 'gym_bro.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      this.db.next(db);
+    });
   }
 
   public async initialise(): Promise<void> {
-    this.db = await this.sqlite.create({
-      name: 'gym_bro.db',
-      location: 'default'
+    this.db.pipe(
+      first()
+    ).subscribe(async (instance: SQLiteObject) => {
+      try {
+        // use batch?
+        this.http.get<string>('assets/db-fixtures.sql').subscribe((res: string) => {
+          console.log('Content read', res);
+        });
+        console.log('Done');
+      } catch (e) {
+        console.log('Caught', e);
+      }
+      // await instance.executeSql(`CREATE TABLE IF NOT EXISTS danceMoves(name VARCHAR(32))`, []);
+      // await instance.executeSql(`INSERT OR IGNORE INTO danceMoves(name) VALUES ('dance1')`, []);
+      // const response = await instance.executeSql(`SELECT name FROM danceMoves`, []);
+      // console.log('Done', response.rows.item(1).name);
     });
-    console.log(1);
-    await this.db.executeSql(`CREATE TABLE IF NOT EXISTS workouts(id INTEGER PRIMARY KEY, date TEXT NOT NULL)`, []);
-    console.log(2);
-    await this.db.executeSql(`CREATE TABLE IF NOT EXISTS exercises(id INTEGER PRIMARY KEY, name TEXT NOT NULL, workout_id REFERENCES workout(id))`, []);
-    console.log(3);
-    await this.db.executeSql(`CREATE TABLE IF NOT EXISTS sets(id INTEGER PRIMARY KEY, reps INTEGER NOT NULL, weight REAL, exercise_id REFERENCES exercise(id))`, []);
-    console.log(4);
-
-    // Add some initial data to work with
-    let result;
-    try {
-      // TODO: find out why this does not execute and 5,6 is not logged
-      result =  await this.db.executeSql(`INSERT INTO workouts (id, date) VALUES (1, 'asdf')`);
-      console.log('Result', result);
-    } catch (e) {
-      console.log('Error', e);
-    } finally {
-      console.log(5);
-    }
-    console.log(6);
-    console.log('Result', result);
-    result = await this.db.executeSql('SELECT id, date FROM workouts');
-    console.log('Result', result);
-    // await this.db.executeSql(`INSERT INTO exercises (name, workout_id) VALUES ('First test workout', 1)`);
-    // await this.db.executeSql(`INSERT INTO sets (reps, weight, exercise_id) VALUES (5, 20, 1)`);
-    // await this.db.executeSql(`INSERT INTO sets (reps, weight, exercise_id) VALUES (4, 25, 1)`);
   }
 }
